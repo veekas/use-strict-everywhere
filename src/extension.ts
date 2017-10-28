d987181bdaf599f7c97c307f43c20d9d2dacea85
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-28 01:41:53 -0400

    adds use strict to active document; will now refactor to modify all relevant directory files

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { ExtensionContext, workspace, WorkspaceEdit, Position } from 'vscode';
+import { ExtensionContext, workspace, SnippetString, Position } from 'vscode'; // WorkspaceEdit,
+// import * as fs from 'fs';
 
-export function activate(context: ExtensionContext) {
+export function activate (context: ExtensionContext) {
 
     // don't do anything if a workspace folder isn't open
-    if (!workspace) return;
+    // if (!workspace) return;
 
     console.log('strict mode activated');
 
@@ -16,22 +17,41 @@ export function activate(context: ExtensionContext) {
 
     let addUseStrict = vscode.commands.registerCommand('extension.use-strict-everywhere', () => {
 
-        let newWorkspaceEdit = new WorkspaceEdit();
+        // let newWorkspaceEdit = new WorkspaceEdit();
+        // let startOfDoc = new Position(0, 0);
+        // let useStrict = `'use strict';\n\n`;
+
+        // // find all .js or .jsx files
+        // workspace.findFiles('/\.jsx?$/')
+        // // insert 'use strict'
+        // .then(files => {
+        //     console.log('these are the files:', files);
+        //     files.map(uri => workspace.applyEdit(newWorkspaceEdit.insert(uri, startOfDoc, useStrict)))
+        // })
+        // // save all files
+        // .then(updatedFiles => workspace.saveAll(true));
+        // .catch(next);
+
+        const currentDoc = vscode.window.activeTextEditor;
+        // const jsFiles = workspace.findFiles('**∕*.js', '**∕node_modules∕**', 10);
+        // const jsFiles = workspace.findFiles(`*.{ts,js,jsx}`);
+        const useStrict = new SnippetString().appendText(`'use strict';\n\n`);
         let startOfDoc = new Position(0, 0);
-        let useStrict = '\'use strict\'\;\n\n'
 
-        // find all .js or .jsx files
-        workspace.findFiles('/\.jsx?$/')
-            // insert 'use strict'
-            .then(files => files.map(uri => workspace.applyEdit(newWorkspaceEdit.insert(uri, startOfDoc, useStrict))))
-            // save all files
-            .then(updatedFiles => workspace.saveAll(false));
+        // // console.log(jsFiles);
+        // workspace.findFiles('**∕*.js', '**∕node_modules∕**', 10)
+        //     .then(file => file.insertSnippet(useStrict, startOfDoc));
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('You are now using strict mode across your workspace.');
+        currentDoc.insertSnippet(useStrict, startOfDoc);
+
+
+
     });
 
     context.subscriptions.push(addUseStrict);
+
+    // Display a message box to the user
+    // vscode.window.showInformationMessage('You are now using strict mode across your workspace.');
 }
 
 // files.map(file =>
